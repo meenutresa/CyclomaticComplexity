2ce9c1219190a468ee5c014eeba94ac296f3f962
meenutresa <meenuvincent1992@gmail.com>
2017-12-01 20:57:15 +0000

    master worker work distribution code

--- master.py
+++ master.py
@@ -2,9 +2,12 @@ import web
 import os
 import MyWebApplication
 import git
+import requests
 
 urls = (
     '/master','master'
+    '/register','register'
+    '/work_done','work_done'
 )
 
 class master:
@@ -13,16 +16,52 @@ class master:
         return
 
     def POST(self):
+        #Give the work to the worker who has approached
+        worker_address = web.input(hostid='',port='')
+        work_details = filelist_per_commit[work_assigned_key]
+        work_assigned_key = work_assigned_key+1
+        url = "http://" + str(worker_address.hostid) + ":"+ str(worker_address.port)+":/worker?id="+str(work_details[0])+"&filename="+str(work_details[1])
+        requests.get(url)
+        return "work assigned"
+class register:
+    #register class for the workers to register
+    def GET(self):
         return
 
+    def POST(self):
+        #regiser the worker
+        no_of_workers = no_of_workers+1
+        return "active"
+class work_done:
+    #register class for the workers to register
+    def GET(self):
+        return
+
+    def POST(self):
+        #get the result from the worker and add to cc_total to get the total
+        #if all the works are done, then the average is taken
+        cyclomatic_complexity = web.input(cc='')
+        work_done_count = work_done_count+1
+        cc_total = cc_total + int(cyclomatic_complexity.cc)
+        if work_done_count == len(filelist_per_commit):
+            cc_average = cc_total/work_done_count
+        return "received result successfully"
+
+no_of_workers = 0
+filelist_per_commit = {}
+work_assigned_key=1
+work_done_count = 0
+cc_total = 0
 if __name__ == "__main__":
     #get the repository and get the commit details. save in a dictionary object
     repo = git.Repo("C:/Users/HP/Documents/GitHub/mlframework")
     commits_list = list(repo.iter_commits('master'))
-    filelist_per_commit = {}
+    i=1
     for commit in commits_list:
-        filelist_per_commit[commit.hexsha] = list(commit.stats.files.keys())
-    print(filelist_per_commit['638d8fc545dc86af94edf1ec57f0102404ef9efe'])
+        for file_key in commit.stats.files.keys():
+            filelist_per_commit[i] = [commit.hexsha,file_key]
+            i=i+1
+    print(len(filelist_per_commit))
 
     app = MyWebApplication.MyWebApplication(urls, globals())
     app.run(port=8080)
--- worker.py
+++ worker.py
@@ -4,6 +4,7 @@ import MyWebApplication
 import lizard
 import sys
 import os
+import requests
 
 urls = (
     '/worker','worker'
@@ -24,13 +25,23 @@ class worker:
         i = lizard.analyze_file(args_passed.filename)
         os.remove(args_passed.filename)
         print(i.average_cyclomatic_complexity)
-        return i.average_cyclomatic_complexity
+        url = "http://localhost:8080/work_done?cc="+str(i.average_cyclomatic_complexity)
+        workdone_response = requests.post(url)
+        #return i.average_cyclomatic_complexity
 
     def POST(self):
         return
 
+
 if __name__ == "__main__":
     #get the port
-    port = int(sys.argv[1])
+    host = sys.argv[1]
+    port = int(sys.argv[2])
     app = MyWebApplication.MyWebApplication(urls, globals())
     app.run(port=port)
+    #register the worker with the master
+    register_response = requests.post("http://localhost:8080/register/")
+    #if the worker is registered, then request for work from the master
+    if register.response == 'active':
+        url = "http://localhost:8080/master?hostid="+str(host)+"&port="+str(port)
+        requests.post(url)
